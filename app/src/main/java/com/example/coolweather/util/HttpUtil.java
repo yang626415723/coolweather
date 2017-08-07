da09be94e906cc93fa1c1a7c0ec9bbc91ca22df2
Tony <tony@gmail.com>
2017-08-07 17:19:58 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by yangjie on 2017/8/6.
+ */
+
+public class HttpUtil {
+    public static void sendOKHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
